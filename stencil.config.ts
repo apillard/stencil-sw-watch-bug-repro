1e093ad34876e790564b2acebc75defdad9c5ffa
Adam Pillard <agentbilly@gmail.com>
2018-08-10 11:52:17 -0500

    Upload implementation of bug repro

---
+++ stencil.config.ts
@@ -0,0 +1,13 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'mycomponent',
+  outputTargets: [
+    {
+      type: 'dist'
+    },
+    {
+      type: 'www'
+    }
+  ]
+};
